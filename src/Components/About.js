9dd1b99f58ff5a03b4f51c284d49b576c06f1605
Cátia <klisar1996@gmail.com>
2021-04-24 15:46:58 +0100

    Add linkedin profile

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,42 +1,60 @@
 import React from "react";
 import "../Style/About.css";
 import { IconContext } from "react-icons";
-import {IoLogoHtml5} from "react-icons/io";
-import {FaCss3Alt} from "react-icons/fa";
-import {DiJavascript} from "react-icons/di";
-import {FaReact} from "react-icons/fa";
+import { IoLogoHtml5 } from "react-icons/io";
+import { FaCss3Alt } from "react-icons/fa";
+import { DiJavascript } from "react-icons/di";
+import { FaReact } from "react-icons/fa";
 import Me from "../Images/Me.jpg";
 import LazyImage from "./LazyImage";
-import placeholderImg from "../placeholder.png"; 
+import placeholderImg from "../placeholder.png";
 
 const About = () => {
-    return (
-        <div className="container">
-             <LazyImage placeholder={placeholderImg} src={Me} alt="Image of Myself" classStyle={"me-img"}/>
-        <div className="about-container">
-            <h1 className="about-title">About Me</h1>
-            <h3 className="about-intro">Find out who I am and what I'm all about</h3>
-            <div className="about-description">
-            <p>My name is Cátia and I´m a junior front-end developer.<br></br>
-            I have a degree on physical therapy and I`m currently working on this area.
-            However, I always had a passion for technology and in the last year I started to expand my knowledge in programming. </p>
-            So now, I want to expand my skills and knowledge and start to put them in action!<br></br>
-            <p>I love to read, do exercise, animals, watch TV series and learn new things.</p>
-            <br></br>
-            <h1 className="about-title">My Skills</h1>
-            <p>I know how to work with <b>HTML</b>, <b>CSS</b> and <b>JavaScript</b>, mainly on <b>React</b> environment. I also can work with jQuery and Sass. </p>
-            <div className="icons-about">
-            <IconContext.Provider value={{size: "3.5em", color: "#474242"}}>
-            <IoLogoHtml5 />
-            <FaCss3Alt />
-            <DiJavascript />
-            <FaReact />
+  return (
+    <div className="container">
+      <LazyImage
+        placeholder={placeholderImg}
+        src={Me}
+        alt="Image of Myself"
+        classStyle={"me-img"}
+      />
+      <div className="about-container">
+        <h1 className="about-title">About Me</h1>
+        <h3 className="about-intro">
+          Find out who I am and what I'm all about
+        </h3>
+        <div className="about-description">
+          <p>My name is Cátia and I´m a junior front-end developer.</p>
+          <p>
+            I have a degree on physical therapy and I`m currently working on
+            this area. However, I always had a passion for technology and in the
+            last two years I started to expand my knowledge in programming. So
+            now, I want to expand my skills and start to put them in action!
+          </p>
+          <p>
+            In my free time I love to read, do exercise, watch TV, be with my
+            friends and learn new things.
+          </p>
+          <br></br>
+          <h1 className="about-title">My Skills</h1>
+          <p>
+            I know how to work with <b>HTML</b>, <b>CSS</b> and
+            <b>JavaScript</b>, mainly on <b>React</b> environment. I also have
+            basic knowledge in <b>jQuery</b>, <b>Sass</b>, <b>C</b> and{" "}
+            <b>Java</b>.
+          </p>
+          <div className="icons-about">
+            <IconContext.Provider value={{ size: "3.5em", color: "#474242" }}>
+              <IoLogoHtml5 />
+              <FaCss3Alt />
+              <DiJavascript />
+              <FaReact />
             </IconContext.Provider>
-            </div>
-            </div>
+          </div>
         </div>
-        </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default About;
--- src/Components/Find.js
+++ src/Components/Find.js
@@ -1,39 +1,50 @@
-import React  from "react";
+import React from "react";
 import "../Style/Find.css";
-import {FaGithub} from "react-icons/fa";
-import {FaFreeCodeCamp} from "react-icons/fa";
-import {FaLinkedin} from "react-icons/fa";
-import {GoMail} from "react-icons/go";
+import { FaGithub } from "react-icons/fa";
+import { FaFreeCodeCamp } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { GoMail } from "react-icons/go";
 import { IconContext } from "react-icons";
 
 const Find = () => {
-  
-  return(
-    
+  return (
     <div className="find-container">
-    <h1 style={{color: "#086577"}}>You can find me on:</h1>
-    <div className="icons">
-    <IconContext.Provider value={{color: "#474242", size: "3.5em"}} >
-    <a href="https://github.com/CatiaAntunes96" target="_blank" rel="noopener noreferrer">
-    <FaGithub />
-    </a>
-    <a href="https://www.freecodecamp.org/catia_antunes" target="_blank" rel="noopener noreferrer">
-    <FaFreeCodeCamp />
-    </a>
-    <FaLinkedin />
-    </IconContext.Provider>
+      <h1 style={{ color: "#086577" }}>You can find me on:</h1>
+      <div className="icons">
+        <IconContext.Provider value={{ color: "#474242", size: "3.5em" }}>
+          <a
+            href="https://github.com/CatiaAntunes96"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub />
+          </a>
+          <a
+            href="https://www.freecodecamp.org/catia_antunes"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaFreeCodeCamp />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/c%C3%A1tia-antunes-b37823210/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+        </IconContext.Provider>
+      </div>
+      <h2 style={{ color: "#086577a1" }}>Or you can contact me:</h2>
+      <span className="icons">
+        <IconContext.Provider value={{ color: "#4b4b4bd1", size: "3.5em" }}>
+          <a href="mailto:catiaantunes8@gmail.com">
+            <GoMail />
+          </a>
+        </IconContext.Provider>
+      </span>
     </div>
-    <h2 style={{color: "#086577a1"}}>Or you can contact me:</h2>
-    <span className="icons">
-    <IconContext.Provider value={{ color: "#4b4b4bd1", size: "3.5em"}} >
-      <a href="mailto:catiaantunes8@gmail.com">
-    <GoMail />
-    </a>
-    </IconContext.Provider>
-    </span>
-    </div>
-    
-    )
-  }
-  
-  export default Find;
+  );
+};
+
+export default Find;
